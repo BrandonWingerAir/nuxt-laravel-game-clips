2ae6297fe8235839eeecb5468b9d6295fa997e80
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-02-09 20:45:55 -0500

    add packages and setup tailwind, pinia & index page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  pages: true,
+  css: ['~/assets/css/main.css'],
+  modules: [
+    'nuxt-icon',
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  build: {
+    transpile: ['pinia-plugin-persistedstate'],
+  },
 })
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-emerald-900 text-white">
+    GameClips
+    <Icon name="fluent-emoji-high-contrast:alien-monster" size="100" class="text-teal-400"/> 
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
