9809431f259dc7090c4bf70afa611ca42edbd41b
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-02-11 17:40:07 -0500

    top header search bar & button colors and borders

---
+++ components/SideNav.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    id="SideNav"
+    :class="rout.fullPath === '/' ? 'lg:w-[310px]' : 'lg:w-[220px]'"
+    class="fixed x-20 bg-white pt-[70px] h-full lg:border-r-0 border-r w-[75px] overflow-auto"
+  >
+    <div class="lg:w-full w-[55px] mx-auto">
+      <NuxtLink iconString="For You"/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+</script>
--- components/TopNav.vue
+++ components/TopNav.vue
@@ -1,31 +1,32 @@
 <template>
   <div
     id="TopNav"
-    class="fixed bg-white z-30 flex items-center w-full border-b h-[61px]"
+    class="fixed z-30 flex items-center w-full bg-neutral-100 border-b h-[70px]"
   >
     <div
       :class="route.fullPath === '/' ? 'max-w-[1150px]' : ''"
-      class="flex items-center jsutify-between w-full px-6 mx-auto"
+      class="flex items-center justify-between w-full px-6 mx-auto"
     >
-      <div :class="route.fullPath === '/' ? 'w-[80%]' : 'lg:w-[20%] w-[70%]'">
-        <NuxtLink to="/" class="flex items-center">
+      <div :class="route.fullPath === '/' ? 'w-[80%]' : 'lg:w-[20%] w-[70%]'" class="flex">
+        <NuxtLink to="/" class="flex items-center mr-6">
           <img width="47" class="mr-2" src="~/assets/images/gameclips-logo.svg">
           <h1 class="mb-1 uppercase text-4xl">
             G<span class="text-lg">ame</span><span class="text-lg font-semibold">Clips</span>
           </h1>
         </NuxtLink>
-      </div>
-
-      <div class="hidden md:flex items-center bg=[#F1F1F2] p-1 rounded-full max-w-[380px] w-full">
-        <input
-          type="text"
-          class="w-full pl-3 my-2 bg-transparent placeholder-[#838383] text-[15px] focus:outline-none"
-          placeholder="Search..."
-        />
-        <div class="px-3 py-1 flex items-center border-l border-l-grey-300">
-          <Icon name="ri:search-line" color="A#A2A7" size="22"/>
+        
+        <div class="hidden md:flex items-center rounded-full max-w-[380px] w-full border-l-2">
+          <input
+            type="text"
+            class="w-full pl-3 py-2 bg-transparent placeholder-neutral-500 text-[15px] focus:outline-none"
+            placeholder="Search..."
+          />
+          <div class="pl-3 pr-5 py-3 flex items-center bg-neutral-700 border-l border-l-grey-300 rounded-r-full">
+            <Icon name="ri:search-line" color="white" size="22"/>
+          </div>
         </div>
       </div>
+
       
       <div class="flex items-center justify-end gap-3 min-w-[275px] max-w-[320px] w-full">
         <!-- <button v-if="false" class="flex items-center border rounded-sm px-3 py-[6px] hover:bg-gray-100">
@@ -45,11 +46,11 @@
         </div> -->
 
         <div class="flex items-center">
-          <Icon class="ml-1 mr-4" name="material-symbols:notifications-unread-outline" color="#161724" size="30"/>
-          <Icon class="mr-5" name="bx:message-detail" color="#161724" size="27"/>
+          <Icon class="ml-1 mr-4" name="bx:message-detail" color="#161724" size="27"/>
+          <Icon class="mr-5" name="material-symbols:notifications-unread-outline" color="#161724" size="30"/>
           <div class="relative">
             <button 
-              class="mt-1"
+              class="mt-1 mr-2"
               @click="$event => showMenu = !showMenu"
             >
               <img
@@ -85,9 +86,9 @@
           </div>
         </div>
         
-        <div class="flex items-center">
-          <button class="flex items-center bg-[#642cf0] text-white border rounded-md px-3 py-[6px]">
-            <Icon name="mdi:plus" color="white" size="25"/>
+        <div class="flex items-center text-white">
+          <button class="flex items-center bg-blue-500 border border-neutral-600 rounded-md px-3 py-[6px]">
+            <Icon name="mdi:plus" size="25"/>
             <span class="px-2 font-medium text-[15px]">
               Upload
             </span>
